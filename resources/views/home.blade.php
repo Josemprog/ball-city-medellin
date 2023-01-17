35c7267a8b6664d841487669edf1ab821f06a911
Jose Murillo <josemprog@gmail.com>
2023-01-17 14:47:12 -0500

    Se creo la estructura base de html con los metadatos correspondientes, el título y el icono de la página.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang=es>
+    <head>
+        <!-- Metadatos -->
+        <meta charset="uft-8">
+        <meta name="author" content="Jose Murillo - josemprog">
+        <meta name="description" content="En este sitio encontrarás lo mejor del baloncesto de Medellín, videos de las mejores jugadas, partidos, torneos, fotos, noticias y todo lo relacionado con el baloncesto de la Ciudad.">
+        <meta name="keywords" content="Baloncesto, Baloncesto de medellín, Ball City, ballcitymedellin, Ball Medellín, City Medellín, Baloncesto Paisa, Baloncesto Colombiano">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- título -->
+        <title>Ball City | Medellín</title>
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="/img/icono-ball-city.ico" type="image/x-icon">
+    </head>
+    <body>
+        
+    </body>
+</html>
