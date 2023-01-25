7f8d82944a1583433b18af4a047b6f3ee607fda4
Jose Murillo <jmurillomu@gmail.com>
2023-01-25 16:00:51 -0500

    build: se creó el modelo Player con su migración y controlador

---
+++ database/migrations/2023_01_25_200620_create_players_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->integer('numero');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('equipo');
+            $table->string('posicion');
+            $table->integer('triples');
+            $table->integer('libres');
+            $table->integer('puntos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+};
